4d5dc4cd209fd23e4f692ef1a95879c3eb0e84f2
Ethan A Kent <ethanallenkent@gmail.com>
2016-02-10 19:35:52 -0800

    Add unitest scaffolding.

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -18,6 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
 
