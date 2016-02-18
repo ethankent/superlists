4d0c7d47e66c0ebc78b5a703b70693e81cb84baa
Ethan A Kent <ethanallenkent@gmail.com>
2016-02-18 14:07:17 -0800

    Add form input validation (prevent blank list items).

--- lists/views.py
+++ lists/views.py
@@ -14,6 +14,8 @@ def view_list(request, list_id):
 
 
 def new_list(request):
+    if not request.POST['item_text']:
+        return render(request, 'home.html', {'error': "You can't have an empty list item"})
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
@@ -21,5 +23,7 @@ def new_list(request):
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if not request.POST['item_text']:
+        return render(request, 'list.html', {'list': list_, 'error': "You can't have an empty list item"})
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
